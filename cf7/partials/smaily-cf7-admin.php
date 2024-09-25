5fda57d5b14195329c406c50d698a9b9edccd317
Kaarel <kaarel.rooparg@gmail.com>
2024-09-25 12:58:40 +0300

    Refactor smaily-cf7-admin.php file to WordPress standards
    Refactors the file to use unified patterns for echoing out strings.

--- cf7/partials/smaily-cf7-admin.php
+++ cf7/partials/smaily-cf7-admin.php
@@ -11,7 +11,7 @@
 <?php if (!isset($_GET['post'])) : ?>
 	<div id='form-id-unknown'>
 		<p id='smailyforcf7-form-id-error' style='padding:15px; background-color:#f2dede; margin:0 0 10px;'>
-			<?php echo esc_html__(
+			<?php esc_html_e(
 				'Configuring Smaily integration is disabled when using "Add New Form". Please save this form or edit an already existing form',
 				'smaily'
 			); ?>
@@ -19,30 +19,32 @@
 	</div>
 <?php else : ?>
 	<p id='smailyforcf7-captcha-error' style='padding:15px; background-color:#ffdf92; margin:0 0 10px; display:<?php echo $has_credentials ? 'none' : 'block'; ?>'>
-		<?php echo esc_html__('Please authenticate smaily credentials under Smaily Settings.', 'smaily'); ?>
+		<?php esc_html_e('Please authenticate smaily credentials under Smaily Settings.', 'smaily'); ?>
 	</p>
 	<div id='smailyforcf7-credentials-valid' style='display:<?php echo $has_credentials ? 'block' : 'none'; ?>'>
 		<p id='smailyforcf7-captcha-error' style='padding:15px; background-color:#ffdf92; margin:0 0 10px; display:<?php echo $captcha_enabled ? 'none' : 'block'; ?>'>
-			<?php echo esc_html__('Captcha disabled. Please use a captcha if this is a public site.', 'smaily'); ?>
+			<?php esc_html_e('Captcha disabled. Please use a captcha if this is a public site.', 'smaily'); ?>
 		</p>
 		<table class='autoresponders-table' style='margin:15px'>
 			<tr class="form-field">
 				<th scope="row" style="text-align:left;padding:10px;">
 					<label for="smaily_status">
-						<?php echo esc_html__('Enable Smaily for this form', 'smaily'); ?>
+						<?php esc_html_e('Enable Smaily for this form', 'smaily'); ?>
 					</label>
 				</th>
 				<td>
-					<input name="smailyforcf7[status]" type="checkbox" <?php checked($is_enabled); ?> class="smaily-toggle" id="smaily_status" value="<?= (int)$is_enabled ?>" />
+					<input name="smailyforcf7[status]" type="checkbox" <?php checked($is_enabled); ?> class="smaily-toggle" id="smaily_status" value="<?php echo (int)$is_enabled ?>" />
 					<label for="smaily_status"></label>
 				</td>
 			</tr>
 			<tr id='smailyforcf7-autoresponders' class='form-field'>
-				<th style="text-align:left;padding:10px;"><?php echo esc_html__('Autoresponder', 'smaily'); ?></th>
+				<th style="text-align:left;padding:10px;">
+					<?php esc_html_e('Autoresponder', 'smaily'); ?>
+				</th>
 				<td>
 					<select id='smailyforcf7-autoresponder-select' name='smailyforcf7-autoresponder'>
 						<option value='' <?php echo $default_autoresponder === 0 ? 'selected="selected"' : ''; ?>>
-							<?php echo esc_html__('No autoresponder', 'smaily'); ?>
+							<?php esc_html_e('No autoresponder', 'smaily'); ?>
 						</option>
 						<?php foreach ($autoresponder_list as $autoresponder_id => $autoresponder_title) : ?>
 							<option value='<?php echo esc_html($autoresponder_id); ?>' <?php if ($default_autoresponder === $autoresponder_id) : ?> selected='selected' <?php endif; ?>>
@@ -54,6 +56,5 @@
 				</th>
 			</tr>
 		</table>
-
 	</div>
 <?php endif; ?>
