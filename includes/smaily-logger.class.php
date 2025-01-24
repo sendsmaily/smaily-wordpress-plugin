0feb0b94fc61fb84aa98659de3b078badc964303
Kaarel <kaarel.rooparg@gmail.com>
2025-01-24 09:10:13 +0200

    Improve logger API

--- includes/smaily-logger.class.php
+++ includes/smaily-logger.class.php
@@ -5,45 +5,60 @@
  */
 class Smaily_Logger {
 
+	const LEVEL_INFO    = 'info';
+	const LEVEL_WARNING = 'warning';
+	const LEVEL_ERROR   = 'error';
+
 	/**
-	 * The table where the logs are stored.
+	 * Table name where logs are stored without the prefix.
 	 *
+	 * @var string
 	 */
-	public static $table_name = 'smaily_logs';
+	const TABLE_NAME = 'smaily_logs';
 
-	public static function create_log_tables() {
-		require_once ABSPATH . 'wp-admin/includes/upgrade.php';
-		global $wpdb;
+	/**
+	 * The service using the logger.
+	 *
+	 * @var string
+	 */
+	protected $service;
 
-		$table_name      = $wpdb->prefix . self::$table_name;
-		$charset_collate = $wpdb->get_charset_collate();
-		$query           = sprintf(
-			"CREATE TABLE %s (
-				log_id int NOT NULL AUTO_INCREMENT,
-				log_level varchar(255) DEFAULT NULL,
-				log_message varchar(255) DEFAULT NULL,
-				log_service varchar(255) DEFAULT NULL,
-				log_time DATETIME DEFAULT '0000-00-00 00:00:00',
-				PRIMARY KEY (log_id)
-				) %s;
-			",
-			$table_name,
-			$charset_collate
-		);
-
-		return maybe_create_table( $table_name, $query );
+	/**
+	 * Constructor for instance-based logging.
+	 * @param string $service
+	 */
+	public function __construct( $service ) {
+		$this->service = $service;
 	}
 
-	public static function drop_log_tables() {
-		global $wpdb;
+	/**
+	 * Log an info message.
+	 *
+	 * @param string $message
+	 * @return void
+	 */
+	public function log_info( $message ) {
+		self::log( $message, self::LEVEL_INFO, $this->service );
+	}
 
-		$table_name = $wpdb->prefix . self::$table_name;
-		$wpdb->query(
-			$wpdb->prepare(
-				'DROP TABLE IF EXISTS %s',
-				$table_name
-			)
-		);
+	/**
+	 * Log a warning message.
+	 *
+	 * @param mixed $message
+	 * @return void
+	 */
+	public function log_warning( $message ) {
+		self::log( $message, self::LEVEL_WARNING, $this->service );
+	}
+
+	/**
+	 * Log an error message.
+	 *
+	 * @param mixed $message
+	 * @return void
+	 */
+	public function log_error( $message ) {
+		self::log( $message, self::LEVEL_ERROR, $this->service );
 	}
 
 	/**
@@ -52,7 +67,7 @@ class Smaily_Logger {
 	 * @param string $message The message to log.
 	 */
 	public static function info( $message, $service = 'general' ) {
-		self::log( $message, 'info', $service );
+		self::log( $message, self::LEVEL_INFO, $service );
 	}
 
 	/**
@@ -61,7 +76,7 @@ class Smaily_Logger {
 	 * @param string $message The message to log.
 	 */
 	public static function warning( $message, $service = 'general' ) {
-		self::log( $message, 'warning', $service );
+		self::log( $message, self::LEVEL_WARNING, $service );
 	}
 
 	/**
@@ -70,7 +85,64 @@ class Smaily_Logger {
 	 * @param string $message The message to log.
 	 */
 	public static function error( $message, $service = 'general' ) {
-		self::log( $message, 'error', $service );
+		self::log( $message, self::LEVEL_ERROR, $service );
+	}
+
+	public static function get_log_messages() {
+		global $wpdb;
+
+		return $wpdb->get_results(
+			$wpdb->prepare(
+				'SELECT * FROM %i ORDER BY log_id DESC',
+				$wpdb->prefix . self::TABLE_NAME
+			),
+			'ARRAY_A'
+		);
+	}
+
+	/**
+	 * Create tables required for storing log messages.
+	 *
+	 * @return bool
+	 */
+	public static function create_log_tables() {
+		require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+		global $wpdb;
+
+		$table_name      = $wpdb->prefix . self::TABLE_NAME;
+		$charset_collate = $wpdb->get_charset_collate();
+		$query           = sprintf(
+			'CREATE TABLE %s (
+				log_id int NOT NULL AUTO_INCREMENT,
+				log_level varchar(255) DEFAULT NULL,
+				log_message TEXT DEFAULT NULL,
+				log_service varchar(255) DEFAULT NULL,
+				log_time DATETIME DEFAULT CURRENT_TIMESTAMP,
+				PRIMARY KEY (log_id)
+				) %s;
+			',
+			$table_name,
+			$charset_collate
+		);
+
+		return maybe_create_table( $table_name, $query );
+	}
+
+	/**
+	 * Drop log tables.
+	 *
+	 * @return void
+	 */
+	public static function drop_log_tables() {
+		global $wpdb;
+
+		$table_name = $wpdb->prefix . self::TABLE_NAME;
+		$wpdb->query(
+			$wpdb->prepare(
+				'DROP TABLE IF EXISTS %s',
+				$table_name
+			)
+		);
 	}
 
 	/**
@@ -90,13 +162,12 @@ class Smaily_Logger {
 		$wpdb->query(
 			$wpdb->prepare(
 				'INSERT INTO %i
-				( log_level, log_message, log_service, log_time )
-				VALUES ( %s, %s, %s, %s )',
-				$wpdb->prefix . self::$table_name,
+				( log_level, log_message, log_service )
+				VALUES ( %s, %s, %s )',
+				$wpdb->prefix . self::TABLE_NAME,
 				$level,
 				$message,
-				$service,
-				current_time( 'mysql' )
+				$service
 			)
 		);
 	}
