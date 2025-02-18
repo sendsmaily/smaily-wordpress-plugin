c8327c7a122e77db6205f9c3d976b8f5003374e5
Kaarel <kaarel.rooparg@gmail.com>
2025-02-18 13:07:06 +0200

    Drop custom messages script

--- admin/js/smaily-admin.js
+++ admin/js/smaily-admin.js
@@ -1,42 +1,5 @@
 'use strict';
 (function ($) {
-	// Display messages handler.
-	function displayMessage(text, error = false) {
-		// Generate message.
-		var message = document.createElement('div')
-		// Add classes based on error / success.
-		if (error) {
-			message.classList.add('error')
-			message.classList.add('smaily-notice')
-			message.classList.add('is-dismissible')
-		} else {
-			message.classList.add('notice-success')
-			message.classList.add('notice')
-			message.classList.add('smaily-notice')
-			message.classList.add('is-dismissible')
-		}
-		var paragraph = document.createElement('p')
-		// Add text.
-		paragraph.innerHTML = text
-		message.appendChild(paragraph)
-		// Close button
-		var button = document.createElement('BUTTON')
-		button.classList.add('notice-dismiss')
-		button.onclick = function () {
-			$(this).closest('div').hide()
-		}
-		message.appendChild(button)
-		// Remove any previously existing messages(success and error).
-		var existingMessages = document.querySelectorAll('.smaily-notice')
-		Array.prototype.forEach.call(existingMessages, function (msg) {
-			msg.remove()
-		})
-		// Inserts message before tabs.
-		document
-			.getElementById('smaily-settings')
-			.insertBefore(message, document.getElementById('tabs'))
-	}
-
 	$().ready(function () {
 		// Generate RSS product feed URL if options change.
 		$('.smaily-rss-options').change(function () {
