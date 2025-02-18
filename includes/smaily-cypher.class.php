a857a9b4970339f85f187550514ebf3d77fd4859
Kaarel <kaarel.rooparg@gmail.com>
2025-02-18 11:25:03 +0200

    Improve cypher comments and logic

--- includes/smaily-cypher.class.php
+++ includes/smaily-cypher.class.php
@@ -9,6 +9,11 @@
 class Smaily_Cypher {
 	const CYPHER = 'AES-256-CBC';
 
+	/**
+	 * Encrypt a string.
+	 * @param string $password
+	 * @return string base64 encoded and encrypted original value.
+	 */
 	public static function encrypt( $password ) {
 		$salt = hash( 'sha256', SECURE_AUTH_KEY );
 		$iv   = substr( AUTH_KEY, 0, openssl_cipher_iv_length( self::CYPHER ) );
@@ -18,7 +23,17 @@ class Smaily_Cypher {
 		return base64_encode( $iv . $hmac . $raw );
 	}
 
+	/**
+	 * Decrypts the cyphertext. Returns empty string when decryption is not possible.
+	 *
+	 * @param string $cyphertext
+	 * @return string decrypted original password.
+	 */
 	public static function decrypt( $cyphertext ) {
+		if ( $cyphertext === '' ) {
+			return '';
+		}
+
 		$salt    = hash( 'sha256', SECURE_AUTH_KEY );
 		$iv_len  = openssl_cipher_iv_length( self::CYPHER );
 		$sha_len = 32;
@@ -28,7 +43,11 @@ class Smaily_Cypher {
 		$hmac    = substr( $decoded, $iv_len, $sha_len );
 		$raw     = substr( $decoded, $iv_len + $sha_len );
 		$pw      = openssl_decrypt( $raw, self::CYPHER, $salt, OPENSSL_RAW_DATA, $iv );
-		$calc    = hash_hmac( 'sha256', $raw, $salt, true );
+		if ( $pw === false ) {
+			return '';
+		}
+
+		$calc = hash_hmac( 'sha256', $raw, $salt, true );
 		if ( hash_equals( $hmac, $calc ) ) {
 			return $pw;
 		}
