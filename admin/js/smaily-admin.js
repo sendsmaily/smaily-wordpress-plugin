4858e266b05f223fc7dd1cf074a08ea6c6bb14b7
Kaarel <kaarel.rooparg@gmail.com>
2025-02-19 12:01:39 +0200

    Add smaily prefix to rss field id-s

--- admin/js/smaily-admin.js
+++ admin/js/smaily-admin.js
@@ -5,22 +5,22 @@
 		$('.smaily-rss-options').change(function () {
 			var rss_url = new URL( smaily_settings['rss_feed_url'] );
 
-			var rss_category = $('#rss-category').val()
+			var rss_category = $('#smaily-rss-category').val()
 			if (rss_category != '') {
 				rss_url.searchParams.set('category', rss_category);
 			}
 
-			var rss_limit = $('#rss-limit').val()
+			var rss_limit = $('#smaily-rss-limit').val()
 			if (rss_limit != '') {
 				rss_url.searchParams.set('limit', rss_limit);
 			}
 
-			var rss_order_by = $('#rss-sort-field').val()
+			var rss_order_by = $('#smaily-rss-sort-field').val()
 			if (rss_order_by != 'none') {
 				rss_url.searchParams.set('order_by', rss_order_by);
 			}
 
-			var rss_order = $('#rss-sort-order').val()
+			var rss_order = $('#smaily-rss-sort-order').val()
 			if (rss_order_by != 'none' && rss_order_by != 'rand') {
 				rss_url.searchParams.set('order', rss_order);
 			}
