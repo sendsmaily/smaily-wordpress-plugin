7c092fedca0111a5d3d3f9fb4b5d18c1b1a2b944
Kaarel <kaarel.rooparg@gmail.com>
2026-06-02 15:25:14 +0300

    Remove window navigation from block
    The blocks now fully live in iframe which complicates window object
    access. Instead the block now renders plain <a> to redirect user to
    plugin settings page. Settings URL also uses admin_url to correctly
    target admin page URL as the globals required by menu_page_url are not
    available durign REST request.

--- blocks/newsletter-signup/src/edit.js
+++ blocks/newsletter-signup/src/edit.js
@@ -93,12 +93,6 @@ export default function Edit( { attributes, setAttributes } ) {
 		} )();
 	}, [ setAttributes ] );
 
-	const handleRedirect = async () => {
-		if ( settingsURL.current ) {
-			window.location.href = settingsURL.current;
-		}
-	};
-
 	if ( autoresponders === null ) {
 		return <Spinner />;
 	}
@@ -108,13 +102,6 @@ export default function Edit( { attributes, setAttributes } ) {
 			<Notice
 				status="error"
 				isDismissible={ false }
-				actions={ [
-					{
-						label: __( 'Go to plugin settings', 'smaily-connect' ),
-						onClick: handleRedirect,
-						variant: 'primary',
-					},
-				] }
 			>
 				<h3>
 					{ __( 'Plugin setup is not complete!', 'smaily-connect' ) }
@@ -125,6 +112,14 @@ export default function Edit( { attributes, setAttributes } ) {
 						'smaily-connect'
 					) }
 				</p>
+				<a target="_top" href={settingsURL.current}>
+					<Button
+						className="smaily-newsletter-block-button-submit"
+						variant="primary"
+					>
+						{__('Go to plugin settings', 'smaily-connect')}
+					</Button>
+				</a>
 			</Notice>
 		);
 	}
--- includes/smaily-api.class.php
+++ includes/smaily-api.class.php
@@ -80,7 +80,7 @@ class API {
 			$configuration,
 			array(
 				'subdomain'    => $credentials['subdomain'],
-				'settings_url' => menu_page_url( $this->plugin_name, false ),
+				'settings_url' => admin_url('admin.php?page=' . $this->plugin_name ),
 			)
 		);
 	}
