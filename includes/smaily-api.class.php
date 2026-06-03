4389d0d647e4ba521e26ea1b726fa6d3783f8455
Kaarel <kaarel.rooparg@gmail.com>
2026-06-03 14:13:41 +0300

    Fix doc comments in API class

--- includes/smaily-api.class.php
+++ includes/smaily-api.class.php
@@ -37,7 +37,6 @@ class API {
 	 * Sets up a new instance of the API.
 	 *
 	 * @param Options $options     Reference to options handler class.
-	 * @param Admin   $admin_model Reference to admin class.
 	 */
 	public function __construct( Options $options, string $plugin_name ) {
 		$this->options     = $options;
@@ -88,7 +87,7 @@ class API {
 	/**
 	 * List available autoresponders to be used as block options.
 	 *
-	 * @return array{label: string, value: string}
+	 * @return list<array{label: string, value: string}>
 	 */
 	public function list_autoresponders() {
 		$autoresponders = Helper::get_autoresponders_list( $this->options );
